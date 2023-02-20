5d195820720906be540772ec71e37d5325d90615
Ainur <sibrinat616@gmail.com>
2023-02-20 16:29:29 +0300

    table work

--- TrainingApp/TrainingScreen.swift
+++ TrainingApp/TrainingScreen.swift
@@ -16,14 +16,24 @@ class TrainingScreen: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
-        let model1 = TrainingCellViewModel(title: "Arm training", imageName: "armMuscle")
+        let model1 = TrainingCellViewModel(title: "Arm ", imageName: "armMuscle")
         tableViewData.append(model1)
         
-        let model2 = TrainingCellViewModel(title: "Legs training", imageName: "legsIcon")
+        let model2 = TrainingCellViewModel(title: "Legs ", imageName: "legsIcon")
         tableViewData.append(model2)
         
-        let model3 = TrainingCellViewModel(title: "Cardio training", imageName: "cardioIcon")
+        let model3 = TrainingCellViewModel(title: "Cardio ", imageName: "cardioIcon")
         tableViewData.append(model3)
+        
+        let model4 = TrainingCellViewModel(title: "ABS ", imageName: "absIcon")
+        tableViewData.append(model4)
+        
+        let model5 = TrainingCellViewModel(title: "Back ", imageName: "backIcon")
+        tableViewData.append(model5)
+        
+        let model6 = TrainingCellViewModel(title: "Warm-up", imageName: "warm-upIcon")
+        tableViewData.append(model6)
+        
 
     }
 }
@@ -42,6 +52,8 @@ extension TrainingScreen: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TrainingCell", for: indexPath) as! TrainingCell
         let model = tableViewData[indexPath.row]
         cell.configure(with: model)
+        
+        
         return cell
     }
 }
